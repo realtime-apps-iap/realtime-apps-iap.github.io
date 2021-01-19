26e2fb3951b871722a43392de2b457832cf1ef6d
Daryll Wong <daryll_wong@hotmail.com>
2021-01-20 01:44:00 +0800

    add session one slides

--- sidebars.js
+++ sidebars.js
@@ -10,6 +10,7 @@ module.exports = {
         "Project: Chat Platform": ["chatapp/intro"],
         //"Project: GPS Location App": ["locationapp/intro", "locationapp/main"],
         "Project: Videocall App": ["videocallapp/intro"],
+        "Session 1": ["sessionone/slides"],
         // Others: ["others/styleguide"], // Not shown in sidebar. Just visit /resources/docs/guide to view this.
     },
     competition: ["competition/info"], // This sidebar is not used anymore.
