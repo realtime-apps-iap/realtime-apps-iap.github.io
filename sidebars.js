c22266825f05b3deafb0af6b6ee94687f429e88f
lowen <lowenhere@gmail.com>
2021-01-22 11:33:36 +0800

    added browserapi tutorial, update chatapp tutorial and added locationapp tutorial objectives

--- sidebars.js
+++ sidebars.js
@@ -8,9 +8,14 @@ module.exports = {
             "introduction/webrtc",
             "introduction/deployment-to-heroku",
         ],
-        "Project: Chat Platform": ["chatapp/intro"],
-        //"Project: GPS Location App": ["locationapp/intro", "locationapp/main"],
-        "Project: Videocall App": ["videocallapp/intro"],
+        "Tutorial: Group Chat App": ["chatapp/intro"],
+        "Tutorial: Video Call App": ["videocallapp/intro"],
+        "Tutorial: Exploring Browser APIs": [
+            "browserapi/intro",
+            "browserapi/main"
+        ],
+        "Tutorial: Location Sharing App": [
+            "locationapp/intro"],
         "Session 1": ["sessionone/slides"],
         // Others: ["others/styleguide"], // Not shown in sidebar. Just visit /resources/docs/guide to view this.
     },
