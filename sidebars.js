9e9641da340db573bd41a3e26de2efe4c8bb470b
Daryll Wong <daryll_wong@hotmail.com>
2021-01-22 13:28:57 +0800

    add placeholder pages

--- sidebars.js
+++ sidebars.js
@@ -6,17 +6,17 @@ module.exports = {
             "introduction/eventdrivenprogramming",
             "introduction/websockets",
             "introduction/webrtc",
+            "introduction/pwa",
             "introduction/deployment-to-heroku",
         ],
         "Tutorial: Group Chat App": ["chatapp/intro"],
         "Tutorial: Video Call App": ["videocallapp/intro"],
-        "Tutorial: Exploring Browser APIs": [
-            "browserapi/intro",
-            "browserapi/main"
-        ],
-        "Tutorial: Location Sharing App": [
-            "locationapp/intro"],
-        "Session 1": ["sessionone/slides"],
+        "Session One": ["sessionone/slides"],
+        "Tutorial: Exploring Browser APIs": ["browserapi/intro", "browserapi/main"],
+        "Tutorial: Location Sharing App": ["locationapp/intro"],
+        "Tutorial: React PWA": ["reactpwa/intro"],
+        "Tutorial: Deploying to Heroku": ["mushroomclassifier/intro"],
+        "Session Two": ["sessiontwo/slides"],
         // Others: ["others/styleguide"], // Not shown in sidebar. Just visit /resources/docs/guide to view this.
     },
     competition: ["competition/info"], // This sidebar is not used anymore.
