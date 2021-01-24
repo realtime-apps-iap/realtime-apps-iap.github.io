b2f09f34ebd44db7020440ffe53df5a0628b7e50
Daryll Wong <daryll_wong@hotmail.com>
2021-01-24 23:20:56 +0800

    Deploy website - based on 73fde397ca0bc188f59dd6439bbba13be067b768

---
+++ 9958fd43.219c4423.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[23],{96:function(e){e.exports=JSON.parse('{"welcome":{"allTagsPath":"/blog/tags","slug":"welcome","name":"welcome","count":2,"permalink":"/blog/tags/welcome"}}')}}]);
